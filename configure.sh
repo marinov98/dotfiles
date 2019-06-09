b98e9fd1c5f7ae0697cb4aa51e2636d63912bd37
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2019-06-09 00:02:05 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,35 +7,40 @@ echo "Copying bash profiles..."
 \cp .bashrc ~/
 \cp .profile ~/
 \cp .bash_profile ~/
+echo "bash done"
 
 # powerline-shell
 echo "setting up powerline-shell"
 mkdir -p ~/.config/powerline-shell 
 cp .config/powerline-shell/config.json ~/.config/powerline-shell/
-
+echo "powerline-shell done"
 
 # FORMATTERS
 echo "Copying formatters..."
 cp .clang-format ~/
 cp .jsbeautifyrc ~/
+echo "formatters done"
 
 # EMACS
 echo "setting up emacs..."
 mkdir -p ~/.emacs.d
 cp .emacs.d/* ~/.emacs.d/
+echo "emacs finished"
 
 # Vim
 echo "setting up Vim..."
 cp .vimrc ~/
 mkdir -p ~/.vim
-cp .vim/* ~/.vim/
+cp .vim/.ycm_extra_conf.py ~/.vim/
+echo "vim finished"
 
-# I3 
+# i3wn 
 echo "setting up i3 window manager..."
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
-cp .config/i3/* ~/.config/i3/
-cp .config/i3status/* ~/.config/i3status/
+cp .config/i3/config ~/.config/i3/
+cp .config/i3status/config ~/.config/i3status/
+echo "i3 finished"
 
 # XORG
 echo "creating xorg configuration..."
@@ -43,3 +48,4 @@ cd
 sudo mkdir -p /etc/X11
 cd dotfiles/
 sudo cp /etc/X11/* /etc/X11/
+echo "xorg finished"
