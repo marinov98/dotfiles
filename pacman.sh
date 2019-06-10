857825e6685af6b9a8eead9efb2c78fbec83565a
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2019-06-10 10:36:19 -0400

    fonts latex and wireless tools

--- pacman.sh
+++ pacman.sh
@@ -6,19 +6,22 @@ echo "Updating before installing packages..."
 sudo pacman -Sy
 
 echo "installing fonts..."
-sudo pacman -S ttf-hack ttf-fira-code adobe-source-code-pro-fonts
+sudo pacman -S ttf-hack ttf-fira-code adobe-source-code-pro-fonts ttf-inconsolata
 
 echo "installing neofeth..."
 sudo pacman -S neofetch
 
 echo "installing git,gdb,curl,ranger, and nnn..."
-sudo pacman -S git gdb curl ranger nnn
+sudo pacman -S git gdb curl ranger nnn wireless_tools
 
 echo "installing keyboard configuration tools..."
-sudo pacman -S xorg-xmodmap xkeycaps interception-caps2esc xcape
+sudo pacman -S xorg-xmodmap xkeycaps interception-caps2esc xcape xorg-xprop xorg-xrandr arandr xorg-xrefresh
 
 echo "installing tools for C++..."
 sudo pacman -S base-devel clang cmake llvm lldb ctags clang-format
+
+echo "x stuff"
+sudo pacman -S xf86-input-libinput xf86-input-wacom xf86-video-fbdev xf86-video-intel xf86-video-vesa xfsprogs xorg-bdftopcf xorg-font-util xorg-fonts-encodings xorg-iceauth xorg-luit xorg-mkfontscale xorg-server xorg-sessreg
 
 echo "installing tools for Python..."
 sudo pacman -S python3 python-pip pyenv
@@ -42,7 +45,7 @@ echo "preparation for i3 configuring..."
 sudo pacman -S feh compton i3-gaps i3status dmenu trash-cli
 
 echo "Latex setup"
-sudo pacman -S texlive-core texline-bin biber
+sudo pacman -S texlive-core texline-bin biber texlive-fontsextra texlive-bibtexextra texlive-formatsextra texlive-science
 
 
 
