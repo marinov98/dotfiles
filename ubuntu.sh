f97141c0da92742236d89d4b514a60f31d167242
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2019-06-11 16:39:00 -0400

    added postgres stuff

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 ## Scipt is to be used with Ubuntu linux distro OR Linux distributions that contain the apt package manager!
 
 echo "updating..."
-sudo apt update
+sudo apt-get update
 
 echo "installing necessities (curl,git,gdb,net-tools,feh,compton,trash-cli)"
 sudo apt install curl git gdb net-tools neofetch feh compton trash-cli 
@@ -11,35 +11,35 @@ curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 
 echo "keyboard configurations..."
 sudo add-apt-repository universe
-sudo apt install gnome-tweak-tool xcape 
+sudo apt-get install gnome-tweak-tool xcape 
 
 echo "fonts..."
-sudo apt install xlappearance fonts-font-awesome 
+sudo apt-get install xlappearance fonts-font-awesome 
 echo "displays..."
-sudo apt install xrandr arandr
+sudo apt-get install xrandr arandr
 
 echo "installing programming essentials..."
-sudo apt install gummi gcc clang libclang-dev lldb ctags libncurses5-dev libncursesw5-dev cmake ranger nnn clang-format python-pip 
+sudo apt-get install gummi gcc clang libclang-dev lldb ctags libncurses5-dev libncursesw5-dev cmake ranger nnn clang-format python-pip 
 echo "python tools using pip..."
 sudo chown -R $(whoami) /usr/local/bin/
 pip install --user virtualenv texlive-full 
 pip install --user rope jedi flake8 autopep8 yapf pygments virtualenvwrapper powerline-shell
 
 echo "javascript..."
-sudo apt install nodejs npm 
-sudo apt-get install postgresql-10
+sudo apt-get install nodejs npm 
+sudo apt-get install postgresql-10 postgresql postgresql-contrib
 echo "npm installations..."
 sudo chown -R $(whoami) ~/.npm
 npm install npm@latest -g
 sudo chown -R $(whoami) /usr/local/lib/node_modules
-npm i -g tern jquery eslint @fortawesome/free-solid-svg-icons http-server react react-dom create-react-app prop-types requirejs typescript express angular reactrap webpack @angular/core
+npm i -g tern jquery eslint @fortawesome/free-solid-svg-icons http-server sequelize-cli react react-dom create-react-app prop-types requirejs typescript express angular reactrap webpack @angular/core
 
 
 echo "emacs installation..."
-sudo apt install emacs26 auctex
+sudo apt-get install emacs26 auctex
 
 echo "battery management..."
-sudo apt install tlp && sudo apt install tlpui
+sudo apt-get install tlp && sudo apt install tlpui
 
 echo "Browsing and music..."
-sudo apt install snapd spotify chromium-browser
+sudo apt-get install snapd spotify chromium-browser
