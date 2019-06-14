04127ebe3d71550a07611ac1441710d06092e3b3
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2019-06-14 10:08:13 -0400

    added express-generator

--- ubuntu.sh
+++ ubuntu.sh
@@ -32,7 +32,7 @@ echo "npm installations..."
 sudo chown -R $(whoami) ~/.npm
 npm install npm@latest -g
 sudo chown -R $(whoami) /usr/local/lib/node_modules
-npm i -g core-js tern pg jquery @fortawesome/free-solid-svg-icons sequelize-cli eslint heroku angular @angular/cli http-server react react-dom create-react-app prop-types express @angular/core typescript tslint requirejs reactstrap webpack
+npm i -g core-js express-generator tern pg jquery @fortawesome/free-solid-svg-icons sequelize-cli eslint heroku angular @angular/cli http-server react react-dom create-react-app prop-types express @angular/core typescript tslint requirejs reactstrap webpack
 
 echo "emacs installation..."
 sudo apt install emacs26 auctex
