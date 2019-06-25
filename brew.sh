babb0ec7e319f6f655e6ebe9143963420d56266e
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2019-06-25 11:58:47 -0400

    pip to python3

--- brew.sh
+++ brew.sh
@@ -14,7 +14,7 @@ brew install reattach-to-user-namespace
 
 echo "installing programming essentials..."
 brew install --with-toolchain llvm
-brew install pip 
+brew install python3
 brew install clang-format gcc ctags aspell yarn 
 brew install cmake node npm gdb neofetch 
 brew install pyenv pyenv-virtualenv pyenv-virtualenvwrapper
