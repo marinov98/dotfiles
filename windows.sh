c34887ac36e3e41e99b4fcc45fb2e0b788dc7b3f
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2019-08-10 17:55:15 -0400

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -6,6 +6,8 @@ set-executionpolicy remotesigned -scope currentuser
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 echo 'installing packages via scoop'
-scoop install git yarn nodejs vim vimtutor
+scoop install git gcc yarn nodejs vim vimtutor curl
+
+
 
 
