8676381699f9d073475fe9b2eada427fa62af65d
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-02-12 14:13:31 -0500

    added alacritty to the mix

--- configure.sh
+++ configure.sh
@@ -44,11 +44,12 @@ cp vim/coc-settings.json ~/.vim/
 echo "vim finished"
 
 # i3wn 
-echo "setting up i3 window manager,powerline-shell,and ranger ..."
-mkdir -p ~/.config/{i3,i3status,powerline-shell,ranger}
+echo "setting up i3 window manager,powerline-shell,alacritty and ranger ..."
+mkdir -p ~/.config/{i3,i3status,powerline-shell,alacritty,ranger}
 cp config/i3/config ~/.config/i3/
 cp config/i3status/config ~/.config/i3status/
 cp config/powerline-shell/config.json ~/.config/powerline-shell/
+cp config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp config/ranger/rc.conf ~/.config/ranger/
 echo "finished"
 
