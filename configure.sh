798131382a651b1f132870fd7b5b20474695fb7a
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-02-26 14:26:33 -0500

    removed npm and pip

--- configure.sh
+++ configure.sh
@@ -64,20 +64,3 @@ echo "Config transfer complete"
 # cd dotfiles/
 # sudo cp /etc/X11/xorg.conf /etc/X11/
 # echo "xocrg finished"
-
-echo "Files copied, now installing packages with pip and npm"
-
-echo "python tools using pip..."
-sudo chown -R $(whoami) /usr/local/bin/
-pip3 install rope jedi pylint flake8 autopep8 yapf pygments virtualenv virtualenvwrapper powerline-shell pynvim
-
-echo "npm installations..."
-
-sudo chown -R $(whoami) ~/.npm
-sudo chown -R $(whoami) /user/lib/node_modules
-sudo chown -R $(whoami) /usr/local/lib/node_modules
-
-
-npm i -g netlify-cli prettier @angular/cli http-server requirejs ngrok 
-echo "installing language servers..."
-npm i -g typescript-language-server vscode-json-languageserver vscode-html-languageserver-bin yaml-language-server vscode-css-languageserver-bin bash-language-server
