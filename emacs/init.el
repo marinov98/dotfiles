a1ae935756e9b19afb7f5a2274da614db069fd89
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-02-28 02:47:51 -0500

    gc settings in init

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,33 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
+
+;; -*- lexical-binding: t; -*-
+
+;;; Temporarily reduce garbage collection during startup. Inspect `gcs-done'.
+(defun reset-gc-cons-threshold ()
+  "Reset garbage collection to default values after startup."
+  (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))
+  (setq gc-cons-percentage 0.1))
+
+(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
+
+(add-hook 'after-init-hook #'reset-gc-cons-threshold)
+
+;;; Temporarily disable the file name handler.
+(setq default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun reset-file-name-handler-alist ()
+  "Reset file name handlers."
+  (setq file-name-handler-alist
+	(append default-file-name-handler-alist
+		file-name-handler-alist))
+  (cl-delete-duplicates file-name-handler-alist :test 'equal))
+
+(add-hook 'after-init-hook #'reset-file-name-handler-alist)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
