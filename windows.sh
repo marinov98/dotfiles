bf378597ef19359e263913ade9a11210792de7ea
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-02-29 00:22:55 -0500

    ag added

--- windows.sh
+++ windows.sh
@@ -8,11 +8,14 @@ iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 echo 'installing packages via scoop'
 scoop install git gcc yarn nodejs vim vimtutor curl make
 scoop install cmake
+scoop install ripgrep 
 
-echo 'installing global yarn packages'
-yarn global add netlify-cli prettier @angular/cli http-server requirejs ngrok
+# uncomment if you need these installs
+# echo 'installing global yarn packages'
+# yarn global add netlify-cli prettier @angular/cli http-server requirejs ngrok
 
 echo 'installing choco`
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install poshgit
+choco install poshgit ag
+
 
