191e0a0e4a767ac029ca63d9c9f1ca477e713b22
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-03-06 23:51:27 -0500

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -6,7 +6,7 @@
 
 ;;; Temporarily reduce garbage collection during startup. Inspect `gcs-done'.
 ;; Link to where I found the code: https://ambrevar.xyz/emacs2/
-(defun reset-gc-cons-threshold ()
+(defun marinov/reset-gc-cons-threshold ()
   "Reset garbage collection to default values after startup."
   (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))
   (setq gc-cons-percentage 0.1))
@@ -14,23 +14,23 @@
 (setq gc-cons-threshold most-positive-fixnum)
 (setq gc-cons-percentage 0.6)
 
-(add-hook 'after-init-hook #'reset-gc-cons-threshold)
+(add-hook 'after-init-hook #'marinov/reset-gc-cons-threshold)
 
 ;;; Temporarily disable the file name handler.
 (setq default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-(defun reset-file-name-handler-alist ()
+(defun marinov/reset-file-name-handler-alist ()
   "Reset file name handlers."
   (setq file-name-handler-alist
 	(append default-file-name-handler-alist
 		file-name-handler-alist))
   (cl-delete-duplicates file-name-handler-alist :test 'equal))
 
-(add-hook 'after-init-hook #'reset-file-name-handler-alist)
+(add-hook 'after-init-hook #'marinov/reset-file-name-handler-alist)
 
 
-(setq load-prefer-newer t) ;; Avoid the pitfall of “loading old bytecode instead of newer source”
+(setq load-prefer-newer t) ;; Avoid the pitfall of ¡°loading old bytecode instead of newer source¡±
 
 (require 'package)
 (setq package-enable-at-startup nil)
