26c6dca6e06dbf133929d06ce6a9536d39131ae4
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2020-03-10 05:45:46 -0400

    rofi added to the configure script

--- configure.sh
+++ configure.sh
@@ -47,12 +47,13 @@ echo "vim finished"
 # i3wn 
 echo "setting up i3 window manager,powerline-shell,alacritty and ranger ..."
 mkdir -p ~/Pictures
-mkdir -p ~/.config/{i3,i3status,powerline-shell,alacritty,ranger}
+mkdir -p ~/.config/{i3,i3status,powerline-shell,alacritty,ranger,rofi}
 cp config/i3/config ~/.config/i3/
 cp config/i3status/config ~/.config/i3status/
 cp config/powerline-shell/config.json ~/.config/powerline-shell/
 cp config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp config/ranger/rc.conf ~/.config/ranger/
+cp config/rofi/config ~/.config/rofi/config
 cp config/i3/img/* ~/Pictures/
 echo "finished"
 
