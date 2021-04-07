268a548a6ddea7dd3abbedb758b5dedc2f83305d
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2021-04-07 14:14:26 -0400

    best fix for npm EACESS

--- npm-pip.sh
+++ npm-pip.sh
@@ -2,9 +2,11 @@ echo "python tools using pip..."
 python -m pip install rope jedi pylint flake8 autopep8 yapf pygments virtualenv virtualenvwrapper powerline-shell pynvim
 
 echo "npm installations..."
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
 
-sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,share}
-
-npm i -g netlify-cli prettier @angular/cli http-server requirejs ngrok 
+npm i -g netlify-cli prettier ngrok 
 echo "installing language servers..."
 npm i -g typescript-language-server vscode-json-languageserver vscode-html-languageserver-bin yaml-language-server vscode-css-languageserver-bin bash-language-server
