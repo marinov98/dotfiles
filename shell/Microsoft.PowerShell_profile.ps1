a5389966ce2d68453cef87b3214f8e4fdcb01a5f
Marin Marinov <wbbmarin5@gmail.com>
2021-08-20 10:19:59 -0400

    added powershell config

---
+++ shell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Set-PSReadLineOption -Colors @{Operator = "DarkCyan"; Parameter = "Red"; Command = "Yellow"; String = "Green"}
