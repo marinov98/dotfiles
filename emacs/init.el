06e8489288f088362c6101e862df6a78234577d5
Marin Marinov <wbbmarin5@gmail.com>
2023-12-05 19:28:36 -0500

    add windows config

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,15 @@
 
 ;; -*- lexical-binding: t; -*-
-(setq load-prefer-newer t) ;; Avoid the pitfall of loading old bytecode instead of newer source
+(setq load-prefer-newer t) ;; Avoid the pitfall of loading old bytecode instead of newer 
+
+;; MS-Windows can be slow and can give a lot of issues. This setting below at least fixes the issue of it not recognizing unicode characters and not letting your save your file.
+(when (string-equal system-type "windows-nt")
+  (prefer-coding-system 'utf-8)
+  (set-language-environment "UTF-8")
+  (set-default-coding-systems 'utf-8)
+  (set-language-environment 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialization Optimization
@@ -33,7 +42,6 @@
 (add-hook 'after-init-hook #'marinov/set-memory)
 (add-hook 'after-init-hook #'marinov/reset-file-name-handler-alist)
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package manager
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -56,3 +64,18 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/MarinMacs.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-default-notes-file (concat org-directory "/Personal/notes.org"))
+ '(org-directory "~/Projects/org")
+ '(package-selected-packages
+   '(gitconfig-mode gitignore-mode company yasnippet-snippets yaml-mode writegood-mode which-key wgrep web-mode wc-mode undo-tree typescript-mode treemacs-projectile treemacs-magit treemacs-evil rjsx-mode react-snippets ranger rainbow-mode quelpa-use-package pretty-hydra prettier-js pdf-tools org-bullets neotree modern-cpp-font-lock lsp-ui lsp-java json-mode graphql-mode git-timemachine exec-path-from-shell evil-surround evil-multiedit evil-mc evil-leader evil-collection emmet-mode doom-themes doom-modeline dockerfile-mode diminish dashboard csv-mode counsel-projectile clang-format+ beacon all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
