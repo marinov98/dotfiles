98db91e80031f5f7245516f4eb942ba6777d89a0
Marin Marinov <wbbmarin5@gmail.com>
2023-12-26 16:12:55 -0500

    improve search function

--- emacs/Components/Search/project-management-tool-setup.el
+++ emacs/Components/Search/project-management-tool-setup.el
@@ -24,8 +24,11 @@
   ;; (add-hook 'project-find-functions #'mpm-project-override)
   (add-hook 'project-find-functions #'mpm-override-dir)
   ;; discover projects when none have been created
-  (when (and (not (file-exists-p (concat user-emacs-directory "projects"))) (file-directory-p mpm-projects-dir))
-    (project-remember-projects-under mpm-projects-dir)))
+  (add-hook 'after-init-hook #'(lambda ()
+                                 (let ((project-list-target-file (concat user-emacs-directory "projects")))
+                                   (when (and (not (file-exists-p project-list-target-file)) (file-directory-p mpm-projects-dir))
+                                     (message (format "'%s' file not found, will attempt to discover projects based on '%s'" project-list-target-file mpm-projects-dir))
+                                     (project-remember-projects-under mpm-projects-dir))))))
 
 (provide 'project-management-tool-setup)
 ;;; project-management-tool-setup.el ends here
