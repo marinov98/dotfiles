58f8c01c9ba8ed1b1da4cb629c4ffe8ac581dee5
Marin Marinov <wbbmarin5@gmail.com>
2023-12-26 12:46:37 -0500

    enhance bindings

--- vim/nvim/lua/marinvim/preferences/keymaps.lua
+++ vim/nvim/lua/marinvim/preferences/keymaps.lua
@@ -2,22 +2,27 @@ vim.g.mapleader = " "
 
 -- saving, quitting
 vim.keymap.set('n', '<leader>s',":w<CR>", { desc = "Save File"})
-vim.keymap.set('n', '<leader>k',":tabclose<CR>", { desc = "Close tab" })
+vim.keymap.set('n', '<leader>k',":bd<CR>", { desc = "Close buffer" })
+vim.keymap.set('n', '<leader>x',":tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set('n', '<leader>q',":q<CR>", { desc = "Quit" })
 vim.keymap.set('n', '<leader>Q',":q!<CR>", { desc = "Force Quit" })
 vim.keymap.set('n', '<leader>n',":noh<CR>", { desc = "Remove highlight" })
 
 -- Buffers and tabs
 vim.keymap.set('n', '<leader>bb',"<C-^>", { desc = "Alternate buffers" })
-vim.keymap.set('n', '<leader>bt',":tabnew<CR>", { desc = "New Tab" })
-vim.keymap.set('n', '<leader>bn',":tabnext<CR>", { desc = "Next Tab" })
-vim.keymap.set('n', '<leader>bp',":tabprevious<CR>", { desc = "Previous Tab" })
+vim.keymap.set('n', '<leader>bp',":bprevious<CR>", { desc = "Previous Buffer" })
+vim.keymap.set('n', '<leader>bn',":bnext<CR>", { desc = "Next buffer" })
+
+vim.keymap.set('n', '<leader>tt',":tabnew<CR>", { desc = "New Tab" })
+vim.keymap.set('n', '<leader>tj',":tabnext<CR>", { desc = "Next Tab" })
+vim.keymap.set('n', '<leader>tk',":tabprevious<CR>", { desc = "Previous Tab" })
 
 -- Coding utility
 vim.keymap.set('n', '<leader>cs',":terminal<CR>", { desc = "Open terminal" })
 vim.keymap.set("n", "<leader>cm", ":%s//gc<Left><Left><Left>") -- attempt at native multiple cursors
+vim.keymap.set("v", "<leader>cm", ":s//gc<Left><Left><Left>") -- attempt at native multiple cursors visual mode
 vim.keymap.set("n", "<leader>*", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- ONLY if hop plugin is used! (These cause error if put inside the plugin itself)
 vim.keymap.set('n', '<leader>ag',":HopChar1<CR>", { desc = "Goto Char 1"})
