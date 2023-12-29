b74976b6bd90ff7d17ab953e22e6aef896cbe9c1
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2023-12-29 11:19:31 -0500

    enable colorizer for all file types

--- vim/nvim/lua/marinvim/plugins/nvim-colorizer.lua
+++ vim/nvim/lua/marinvim/plugins/nvim-colorizer.lua
@@ -1,15 +1,5 @@
 return {
   "NvChad/nvim-colorizer.lua",
   version = "*",
-  config = function()
-    require('colorizer').setup {
-      filetypes = {
-        'css',
-        'javascript',
-        'typescript',
-        'html',
-        "scss"
-      },
-    }
-  end,
+  config = true
 }
