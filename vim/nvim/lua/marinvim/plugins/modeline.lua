da75ea9e0eea0888ad63207a9d1f37ebde3745f6
Marin Marinov <wbbmarin5@gmail.com>
2024-01-01 00:33:20 -0500

    configure modeline a little bit

--- vim/nvim/lua/marinvim/plugins/modeline.lua
+++ vim/nvim/lua/marinvim/plugins/modeline.lua
@@ -3,8 +3,16 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = 'catppuccin'
-      }
+        theme = 'catppuccin',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = {},
+        lualine_z = {}
+      },
     })
   end
 }
