34dcdf987c38d2745c2a9cda0cf9018eaa851151
Marin Marinov <wbbmarin5@gmail.com>
2024-01-01 10:51:37 -0500

    global status and fileformat instead of type

--- vim/nvim/lua/marinvim/plugins/modeline.lua
+++ vim/nvim/lua/marinvim/plugins/modeline.lua
@@ -4,12 +4,13 @@ return {
     require("lualine").setup({
       options = {
         theme = 'catppuccin',
+        globalstatus = true,
       },
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', { 'filetype', icons_enabled = false } },
+        lualine_x = { 'encoding', { 'fileformat', icons_enabled = false } },
         lualine_y = {},
         lualine_z = {}
       },
