797eb8787516fda52580aa49a610db02d79c31b2
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-01 23:12:21 -0500

    make sure languages are hooked on eglot

--- config/emacs/backups/alt-Configs/experimental/vertico-project-eglot-flymake/Components/Programming/lsp-setup.el
+++ config/emacs/backups/alt-Configs/experimental/vertico-project-eglot-flymake/Components/Programming/lsp-setup.el
@@ -5,7 +5,9 @@
 
 ;;; Code:
 (use-package eglot
-  :hook (python-ts-mode . eglot-ensure)
+  :hook
+  (((c++-mode c-mode css-mode yaml-mode json-mode js-mode js2-mode rjsx-mode typescript-mode web-mode) . eglot-ensure)
+   ((bash-ts-mode rust-ts-mode go-ts-mode css-ts-mode yaml-ts-mode json-ts-mode js-ts-mode typescript-ts-mode tsx-ts-mode) . eglot-ensure)) ;; treesitter modes
   :bind (:map evil-normal-state-map  ;; gd and gr seem to automatically map appropriately to xref find definitions and references
               ("gi" . eglot-find-implementation)
               ("gy" . eglot-find-typeDefinition)))
