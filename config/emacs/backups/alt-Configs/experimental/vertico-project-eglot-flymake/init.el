3a4d4de4a9b6c9546aac2252da9771f21b5ae0b7
Marin Marinov <wbbmarin5@gmail.com>
2024-01-01 22:35:51 -0500

    update dir

--- config/emacs/backups/alt-Configs/experimental/vertico-project-eglot-flymake/init.el
+++ config/emacs/backups/alt-Configs/experimental/vertico-project-eglot-flymake/init.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 ;; in case early-init doesn't load (Emacs 27 introduced early-init)
 (when (< emacs-major-version 27)
- (load "~/.emacs.d/early-init.el"))
+ (load (concat user-emacs-directory "early-init.el")))
 
 ;;; Code:
 ;; load our custom components except startup (used in early-init.el)
@@ -19,17 +19,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-default-notes-file (concat org-directory mpm-notes-org))
- '(org-directory mpm-org-dir)
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
