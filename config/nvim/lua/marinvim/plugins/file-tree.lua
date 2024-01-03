40b8fc0edea67831ef2196276f947d3c54cf438a
Marin Marinov <wbbmarin5@gmail.com>
2024-01-02 20:42:56 -0500

    remove icons

--- config/nvim/lua/marinvim/plugins/file-tree.lua
+++ config/nvim/lua/marinvim/plugins/file-tree.lua
@@ -3,7 +3,6 @@ return  {
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
     config = function()
