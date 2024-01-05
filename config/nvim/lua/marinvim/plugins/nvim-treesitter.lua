90612bff134917ec3167a90295683b22a651b693
Marin Marinov <wbbmarin5@gmail.com>
2024-01-05 11:02:38 -0500

    add recommendations by documentation

--- config/nvim/lua/marinvim/plugins/nvim-treesitter.lua
+++ config/nvim/lua/marinvim/plugins/nvim-treesitter.lua
@@ -2,10 +2,24 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "lua", "python", "elixir", "typescript", "html" },
+        ensure_installed = {
+          -- mandatory (keep these as recommended by documentation)
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          -- preference (personal choice starts here)
+          "python",
+          "elixir",
+          "javascript",
+          "typescript",
+          "html"
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
