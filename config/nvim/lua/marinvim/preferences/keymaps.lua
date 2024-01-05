e8839b0b614c20f68852bececd5b925e0bb88e44
Marin Marinov <wbbmarin5@gmail.com>
2024-01-05 11:59:12 -0500

    improved git workflow

--- config/nvim/lua/marinvim/preferences/keymaps.lua
+++ config/nvim/lua/marinvim/preferences/keymaps.lua
@@ -29,5 +29,6 @@ vim.keymap.set("n", "<leader><leader>k", "<cmd>%bd|e#<cr>", { desc = "Close all 
 -- Git
 vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = "Launch Fugitive" })
 vim.keymap.set('n', '<leader>gs', ":Git ", { desc = "Git commands" })
-vim.keymap.set('n', '<leader>gj', ":diffget //3", { desc = "diff get 3" })
-vim.keymap.set('n', '<leader>gf', ":diffget //2", { desc = "diff get 2" })
+vim.keymap.set('n', '<leader>gm', ":Gdiffsplit!<CR>", { desc = "Open 3 way split" })
+vim.keymap.set('n', '<leader>gj', ":diffget //3<CR>", { desc = "diff get 3" })
+vim.keymap.set('n', '<leader>gf', ":diffget //2<CR>", { desc = "diff get 2" })
