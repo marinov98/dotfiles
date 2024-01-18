01b435c7d012b0483ad374df013113d8edd6a406
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-01-18 18:13:45 -0500

    fix binding mistake and add dap

---
+++ config/nvim/lua/marinvim/plugins/dap-setup.lua
@@ -0,0 +1,30 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+  },
+  cmd = { 'DapUIToggle', 'DapToggleRepl', 'DapToggleBreakpoint' },
+  config = function()
+    require("dapui").setup()
+    -- Experimental need to add dap servers for this to work!
+    local dap, dapui = require("dap"), require("dapui")
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
+}
