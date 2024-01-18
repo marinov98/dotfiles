3a5f9b32d48d26e5557b7476593d259d281034e0
Marin Marinov <wbbmarin5@gmail.com>
2024-01-18 18:07:28 -0500

    Neotree command option

--- config/nvim/lua/marinvim/plugins/file-tree.lua
+++ config/nvim/lua/marinvim/plugins/file-tree.lua
@@ -6,17 +6,18 @@ return  {
       'nvim-tree/nvim-web-devicons',
       "MunifTanjim/nui.nvim",
     },
+    cmd = "Neotree",
     keys = '<leader>tn',
     config = function()
       require("neo-tree").setup({
-      filesystem = {
+        filesystem = {
           filtered_items = {
-            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            visible = true,
             hide_dotfiles = false,
             hide_gitignored = false,
           },
         }
       })
-      vim.keymap.set('n', '<leader>tn',":Neotree toggle<CR>", {})
+      vim.keymap.set('n', '<leader>tn', ":Neotree toggle<CR>", {})
     end
 }
