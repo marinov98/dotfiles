fde7d6c033bf1e490f8f88661fd4407b36352593
Marin Marinov <wbbmarin5@gmail.com>
2024-01-19 12:33:02 -0500

    add aliases

--- shell/Microsoft.PowerShell_profile.ps1
+++ shell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,8 @@
+# Modules
 Import-Module posh-git
 Set-PSReadLineOption -Colors @{Operator = "DarkCyan"; Parameter = "Red"; Command = "Yellow"; String = "Green"}
+
+# Alias
+Set-Alias vim v
+Set-Alias grep findstr
+Set-Alias ll ls
