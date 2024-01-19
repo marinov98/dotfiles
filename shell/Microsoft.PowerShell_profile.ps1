ab2769f0e13e7e4e11bce3aa491bbc37d876ef42
Marin Marinov <wbbmarin5@gmail.com>
2024-01-19 12:40:52 -0500

    add which command

--- shell/Microsoft.PowerShell_profile.ps1
+++ shell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,19 @@
 # Modules
 Import-Module posh-git
+
+# PSReadonline
 Set-PSReadLineOption -Colors @{Operator = "DarkCyan"; Parameter = "Red"; Command = "Yellow"; String = "Green"}
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
 
 # Alias
 Set-Alias vim v
 Set-Alias grep findstr
 Set-Alias ll ls
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
