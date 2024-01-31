7ee9d4a55372a06baa6b54883b31c72b96ffc079
Marin Marinov <wbbmarin5@gmail.com>
2024-01-30 22:26:27 -0500

    lsp plists

--- config/emacs/backups/alt-Configs/experimental/vertico-project-config/early-init.el
+++ config/emacs/backups/alt-Configs/experimental/vertico-project-config/early-init.el
@@ -1,6 +1,7 @@
 ;; early-init.el -- startup before loading GUI  -*- lexical-binding: t; -*-
 
 (setq load-prefer-newer t) ;; Avoid the pitfall of loading old bytecode instead of newer
+;; (setenv "LSP_USE_PLISTS" "true") ;; supposed to make lsp faster
 
 ;;; Commentary:
 ;; MS-Windows can be slow and can give a lot of issues. This setting below at least fixes the issue of it not recognizing unicode characters and not letting your save your file.
