9d0cef8afeb162afa50ca0b8baf64ba009c2433e
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-12-26 17:37:02 -0500

    use devicons instead of mini for now as mini wants too much config

--- config/nvim/lua/marinvim/plugins/code-custom-utility.lua
+++ config/nvim/lua/marinvim/plugins/code-custom-utility.lua
@@ -7,14 +7,40 @@ return {
   },
   -- icons
   {
-    'echasnovski/mini.icons',
-    version = '*',
+    'nvim-tree/nvim-web-devicons',
+    version = false,
     opts = {}
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = '<leader>tn',
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        }
+      })
+      vim.keymap.set('n', '<leader>tn', ":Neotree toggle<CR>", {})
+    end
   },
   {
     'stevearc/oil.nvim',
     version = "*",
     config = function()
+      -- ensure netrw is disabled
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
       require("oil").setup({
         view_options = {
           show_hidden = true
