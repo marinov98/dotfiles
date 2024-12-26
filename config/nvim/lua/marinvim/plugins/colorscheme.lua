9a1ddab362fdf459d3b2c7a563deaaeeaf1a6212
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2024-12-26 13:18:27 -0500

    add tokyo night theme

--- config/nvim/lua/marinvim/plugins/colorscheme.lua
+++ config/nvim/lua/marinvim/plugins/colorscheme.lua
@@ -2,11 +2,21 @@ return {
   {
     "catppuccin/nvim",
     lazy = false,
+    enabled = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
       require('catppuccin').setup({ flavour = "mocha" })
       vim.cmd.colorscheme("catppuccin")
     end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({ style = "moon" })
+      vim.cmd.colorscheme("tokyonight")
+    end,
   }
 }
