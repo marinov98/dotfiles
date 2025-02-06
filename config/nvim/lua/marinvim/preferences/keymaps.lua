9ae000da551f5e44e4a78307e7fd302163416864
Marin Marinov <mmarinov7@gatech.edu>
2025-02-05 20:52:08 -0500

    make sure to sync keymaps also

--- config/nvim/lua/marinvim/preferences/keymaps.lua
+++ config/nvim/lua/marinvim/preferences/keymaps.lua
@@ -1,12 +1,14 @@
 vim.g.mapleader = " "
 
--- saving, quitting, copying, pasting
+-- saving, quitting
 vim.keymap.set('n', '<leader>s', ":w<CR>", { desc = "Save File" })
 vim.keymap.set('n', '<leader>k', ":bd<CR>", { desc = "Close buffer" })
 vim.keymap.set('n', '<leader>x', ":tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set('n', '<leader>q', ":q<CR>", { desc = "Quit" })
 vim.keymap.set('n', '<leader>Q', ":q!<CR>", { desc = "Force Quit" })
 vim.keymap.set('n', '<leader>n', ":noh<CR>", { desc = "Remove highlight" })
+
+ -- copying, pasting
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy line to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
@@ -21,13 +23,6 @@ vim.keymap.set("n", "<leader>i", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>uc", ":! ", { silent = true, desc = "Execute external command" })
 vim.keymap.set("n", "<leader><leader>k", "<cmd>%bd|e#<cr>", { desc = "Close all buffers but the current one" }) -- https://stackoverflow.com/a/42071865/516188
-
--- Git
-vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = "Launch Fugitive" })
-vim.keymap.set('n', '<leader>gs', ":Git ", { desc = "Git commands" })
-vim.keymap.set('n', '<leader>gm', ":Gdiffsplit!<CR>", { desc = "Open 3 way split" })
-vim.keymap.set('n', '<leader>gdj', ":diffget //2<CR>", { desc = "diff get 2" })
-vim.keymap.set('n', '<leader>gdk', ":diffget //3<CR>", { desc = "diff get 3" })
 
 -- File tree 
 -- vim.keymap.set('n', '-', vim.cmd.Explore, { desc = "Open file browser" }) -- Uncomment when not using a different file tree plugin
