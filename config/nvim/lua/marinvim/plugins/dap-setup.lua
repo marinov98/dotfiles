82bd9213bd9f09c3b285f2d8c86d931de11ca45f
Marin Marinov <mmarinov7@gatech.edu>
2025-02-08 13:48:22 -0500

    <Leader> -> <leader> dap

--- config/nvim/lua/marinvim/plugins/dap-setup.lua
+++ config/nvim/lua/marinvim/plugins/dap-setup.lua
@@ -22,9 +22,9 @@ return {
       dapui.close()
     end
 
-    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+    vim.keymap.set("n", "<leader>dt", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<leader>do", ":DapStepOver<CR>")
   end,
 }
