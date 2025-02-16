8a79986deb64a521f872fd3ec60941088f275c20
Marin Marinov <mmarinov7@gatech.edu>
2025-02-16 01:58:27 -0500

    tree mapping

--- config/nvim/lua/marinvim/preferences/keymaps.lua
+++ config/nvim/lua/marinvim/preferences/keymaps.lua
@@ -25,4 +25,4 @@ vim.keymap.set("n", "<leader><leader>k", "<cmd>%bd|e#<cr>", { desc = "Close all 
 
 -- File tree 
 -- vim.keymap.set('n', '-', vim.cmd.Explore, { desc = "Open file browser" }) -- Uncomment when not using a different file tree plugin
--- vim.keymap.set('n', '<leader>tn', ":Vexplore!<CR>", { desc = "Open netrw side bar" }) -- same as above but open as a sidebar
+-- vim.keymap.set('n', '<leader>ut', ":Vexplore!<CR>", { desc = "Open netrw side bar" }) -- same as above but open as a sidebar
