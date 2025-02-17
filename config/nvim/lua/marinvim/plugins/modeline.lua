38f1975226087c164a6393c42661bb47fd6231e6
Marin Marinov <mmarinov7@gatech.edu>
2025-02-17 15:41:12 -0500

    happy with mini.statusline?

--- config/nvim/lua/marinvim/plugins/modeline.lua
+++ config/nvim/lua/marinvim/plugins/modeline.lua
@@ -1,30 +1,6 @@
 return {
   {
-    "nvim-lualine/lualine.nvim",
-    enabled = false,
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = 'tokyonight',
-          component_separators = { left = "", right = "" },
-          globalstatus = true,
-        },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = {},
-          lualine_c = { { 'filename', path = 1 }, 'diagnostics' },
-          lualine_x = { 'encoding', 'filetype', 'location' },
-          lualine_y = {},
-          lualine_z = {}
-        },
-      })
-      vim.opt.showmode = false -- avoid duplicate showings of mode
-      vim.opt.showcmd = false  -- don't show commands'
-    end
-  },
-  {
     'echasnovski/mini.statusline',
-    enabled = true,
     version = false,
     config = function()
       require("mini.statusline").setup({
@@ -34,7 +10,7 @@ return {
             local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 80 })
             local lsp           = MiniStatusline.section_lsp({ trunc_width = 80 })
             local filename      = MiniStatusline.section_filename({ trunc_width = 120 })
-            -- local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
             local location      = MiniStatusline.section_location({ trunc_width = 200 })
 
             return MiniStatusline.combine_groups({
@@ -42,11 +18,11 @@ return {
               { hl = 'MiniStatuslineFilename', strings = { filename } },
               { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics } },
               { hl = 'MiniStatuslineFilename', },
-              '%<', -- Mark general truncate point
+              -- '%<', -- Mark general truncate point
               '%=', -- End left alignment
               { hl = 'MiniStatuslineDevinfo',  strings = { lsp } },
-              { hl = 'MiniStatuslineFilename', strings = { location } },
-              -- { hl = 'MiniStatuslineFileinfo', strings = { fileinfo, location } },
+              { hl = 'MiniStatuslineFilename', strings = { fileinfo } },
+              { hl = 'MiniStatuslineFileinfo', strings = { location } },
             })
           end
         }
