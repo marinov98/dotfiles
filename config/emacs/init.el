de53cd326083b0a82273677f4afca38780e94441
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2025-09-14 14:15:49 -0400

    remove custom set in init

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -17,15 +17,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
