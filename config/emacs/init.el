01a27509127b5e11bb2de1ec2a6590a95046a65a
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2025-09-15 00:48:19 -0400

    refactor: clean init

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -24,16 +24,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-directory mpm-org-dir)
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
