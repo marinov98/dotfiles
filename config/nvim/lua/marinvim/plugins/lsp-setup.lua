dd65160be3f32aadba8ac5ca0f61878a7810f8e4
Marin Marinov <mmarinov7@gatech.edu>
2025-09-19 23:03:41 -0400

    new lsp config

--- config/nvim/lua/marinvim/plugins/lsp-setup.lua
+++ config/nvim/lua/marinvim/plugins/lsp-setup.lua
@@ -17,80 +17,83 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = { 'saghen/blink.cmp' },
-    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = { "saghen/blink.cmp" },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local lspconfig = require('lspconfig')
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local servers = {
+        "pyright",
+        "ts_ls",
+        "jsonls",
+        "yamlls",
+        "html",
+        "cssls",
+        "gopls",
+        "rust_analyzer",
+        "elixirls",
+      }
 
-      -- Servers
-      lspconfig.lua_ls.setup({
+      for _, server in ipairs(servers) do
+        vim.lsp.config[server] = vim.tbl_deep_extend("force", vim.lsp.config[server] or {}, {
+          capabilities = capabilities,
+        })
+      end
+
+      -- Special config for lua_ls
+      vim.lsp.config.lua_ls = vim.tbl_deep_extend("force", vim.lsp.config.lua_ls or {}, {
         capabilities = capabilities,
         settings = {
           Lua = {
             diagnostics = {
-              globals = { "vim", "it", "describe", "Snacks" }
-            }
-          }
-        }
+              globals = { "vim", "it", "describe", "Snacks", "MiniStatusline" },
+            },
+          },
+        },
       })
 
-      lspconfig.pyright.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
-      lspconfig.jsonls.setup({ capabilities = capabilities })
-      lspconfig.yamlls.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities })
-      lspconfig.cssls.setup({ capabilities = capabilities })
-      lspconfig.gopls.setup({ capabilities = capabilities })
-      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-      lspconfig.elixirls.setup({ capabilities = capabilities })
+      -- Finally enable them all
+      vim.lsp.enable(vim.list_extend({ "lua_ls" }, servers))
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      -- Keymaps / autocmd on attach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
           local local_buf = ev.buf
 
-          -- Diagnostic
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Next diagnostic in current file" })
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Previous diagnostic in current file" })
-          vim.keymap.set('n', '<leader>dg', vim.diagnostic.open_float, { desc = "Glance Diagnostic" })
+          -- Diagnostics navigation
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic", buffer = local_buf })
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic", buffer = local_buf })
+          vim.keymap.set("n", "<leader>dg", vim.diagnostic.open_float, { desc = "Glance Diagnostic", buffer = local_buf })
 
-          -- L (lsp)
-          vim.keymap.set('n', '<leader>lg', vim.lsp.buf.hover, { desc = "LSP Glance", buffer = local_buf })
-          vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = "LSP Code Action", buffer = local_buf })
-          vim.keymap.set('n', '<leader>lF', function()
-              vim.lsp.buf.format { async = true }
-          end, { desc = "LSP format file", buffer = local_buf })
+          -- LSP utilities
+          vim.keymap.set("n", "<leader>lg", vim.lsp.buf.hover, { desc = "LSP Hover", buffer = local_buf })
+          vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Code Action", buffer = local_buf })
+          vim.keymap.set("n", "<leader>lF", function()
+            vim.lsp.buf.format({ async = true })
+          end, { desc = "Format Document", buffer = local_buf })
 
-          vim.keymap.set('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder", buffer = local_buf })
-          vim.keymap.set('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder", buffer = local_buf })
+          vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder,
+            { desc = "Add WS Folder", buffer = local_buf })
+          vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder,
+            { desc = "Remove WS Folder", buffer = local_buf })
 
-          vim.keymap.set('n', 'grd', vim.lsp.buf.declaration, { desc = "Go to declaration", buffer = local_buf })
-          vim.keymap.set('n', 'grn', vim.lsp.buf.rename, { desc = "Rename variable", buffer = local_buf })
-          vim.keymap.set('n', 'grh', vim.lsp.buf.signature_help, { desc = "Signature Help", buffer = local_buf })
-          vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, { desc = "Signature Help", buffer = local_buf })
+          vim.keymap.set("n", "grd", vim.lsp.buf.declaration, { desc = "Go to Declaration", buffer = local_buf })
+          vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename", buffer = local_buf })
+          vim.keymap.set("n", "grh", vim.lsp.buf.signature_help, { desc = "Signature Help", buffer = local_buf })
+          vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "Signature Help", buffer = local_buf })
 
-          -- Original
-          -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Show Definitions", buffer = local_buf })
-          -- vim.keymap.set('n', 'grr', vim.lsp.buf.references, { desc = "Show References", buffer = local_buf })
-          -- vim.keymap.set('n', 'gri', vim.lsp.buf.implementation, { desc = "Show implementations", buffer = local_buf })
-          -- vim.keymap.set('n', 'gry', vim.lsp.buf.type_definition, { desc = "Show Type Definitions", buffer = local_buf })
-          -- vim.keymap.set('n', '<leader>ls', vim.lsp.buf.document_symbol, { desc = "Document Symbol", buffer = local_buf })
-          -- vim.keymap.set('n', '<leader>lws', vim.lsp.buf.workspace_symbol, { desc = "Workspace Symbols", buffer = local_buf })
-
-          -- Picker Variant
+          -- Picker variant
           local picker = Snacks.picker
-          vim.keymap.set('n', 'gd', picker.lsp_definitions, { desc = "Show Definitions", buffer = local_buf })
-          vim.keymap.set('n', 'grr', picker.lsp_references, { desc = "Show References", buffer = local_buf })
-          vim.keymap.set('n', 'gri', picker.lsp_implementations, { desc = "Show implementations", buffer = local_buf })
-          vim.keymap.set('n', 'gry', picker.lsp_type_definitions, { desc = "Show Type Definitions", buffer = local_buf })
-          vim.keymap.set('n', '<leader>ls', picker.lsp_symbols, { desc = "Document Symbol", buffer = local_buf })
-          vim.keymap.set('n', '<leader>lws', picker.lsp_workspace_symbols, { desc = "Workspace Symbols", buffer = local_buf })
+          vim.keymap.set("n", "gd", picker.lsp_definitions, { desc = "Definitions", buffer = local_buf })
+          vim.keymap.set("n", "grr", picker.lsp_references, { desc = "References", buffer = local_buf })
+          vim.keymap.set("n", "gri", picker.lsp_implementations, { desc = "Implementations", buffer = local_buf })
+          vim.keymap.set("n", "gry", picker.lsp_type_definitions, { desc = "Type Definitions", buffer = local_buf })
+          vim.keymap.set("n", "<leader>ls", picker.lsp_symbols, { desc = "Document Symbols", buffer = local_buf })
+          vim.keymap.set("n", "<leader>lws", picker.lsp_workspace_symbols,
+            { desc = "Workspace Symbols", buffer = local_buf })
         end,
       })
-    end
+    end,
   }
 }
