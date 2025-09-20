6f43fa4c4d1e8583c3bfa3f36952801e765303c9
marinov98 <wbbmarin5@gmail.com>
2025-09-19 23:59:38 -0400

    fix modus theme issues and standardize theme config

--- config/emacs/mpm-config/themes-setup.el
+++ config/emacs/mpm-config/themes-setup.el
@@ -9,32 +9,38 @@
 
 (use-package doom-themes
      :disabled
+     :ensure t
      :custom
      (doom-themes-enable-bold t)
      (doom-themes-enable-italic t)
      :config
      (load-theme 'doom-dracula t)
      (doom-themes-visual-bell-config) ;; Enable flashing mode-line on errors
-     (doom-themes-org-config)) ;; Corrects (and improves) org-mode's native fontification.
+     (doom-themes-org-config) ;; Corrects (and improves) org-mode's native fontification.
+)
 
- (use-package modus-vivendi-theme
-     :disabled
-     :ensure t
-     :config
-     (setq modus-vivendi-theme-bold-constructs t)
-     (load-theme 'modus-vivendi t))
+(use-package modus-themes
+    :disabled
+    :ensure t
+    :config
+    (setq modus-themes-bold-constructs t)
+    (load-theme 'modus-vivendi-tinted t)
+)
 
- (use-package catppuccin-theme
+(use-package catppuccin-theme
+    ;; :disabled
     :ensure t
     :custom
     (catppuccin-flavor 'macchiato)
     :config
-    (load-theme 'catppuccin :no-confirm))
+    (load-theme 'catppuccin :no-confirm)
+)
 
 ;; Modeline
 (use-package doom-modeline
-      :ensure t
-      :hook (after-init . doom-modeline-mode))
+    :ensure t
+    :hook (after-init . doom-modeline-mode)
+)
 
 (provide 'themes-setup)
 ;;; themes-setup.el ends here
