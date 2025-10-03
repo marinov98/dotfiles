87339e7d67aa3b3bb12341754e408f238185a829
Marin Marinov <mmarinov7@gatech.edu>
2025-10-02 23:10:17 -0400

    feat(nvim): fuzzy finder with config

--- config/nvim/lua/marinvim/plugins/fuzzy-finder.lua
+++ config/nvim/lua/marinvim/plugins/fuzzy-finder.lua
@@ -3,75 +3,82 @@ return {
     "folke/snacks.nvim",
     lazy = false,
     priority = 1001,
-    opts = {
-      picker = {
-        enabled = true,
-        layout = {
+    config = function()
+      local Snacks = require("snacks")
+
+      local ivy_layout = { preset = "ivy", layout = { height = 0.3 } }
+
+      Snacks.setup({
+        picker = {
+          enabled = true,
           layout = {
-            backdrop = false
-          }
-        },
-        sources = {
-          projects = {
-            filter = {
-              paths = { ["~/projects"] = true },
+            layout = {
+              backdrop = false,
+              height = 0.5,
+            },
+          },
+          sources = {
+            projects = {
+              filter = {
+                paths = { ["~/projects"] = true },
+              },
+            },
+            buffers = {
+              layout = ivy_layout,
+            },
+            diagnostics = {
+              layout = ivy_layout,
+            },
+            lines = {
+              layout = ivy_layout
+            },
+            grep_buffers = {
+              layout = ivy_layout
+            },
+            lsp_symbols = {
+              layout = ivy_layout
             }
           },
-          files = {
-            hidden = true
-          },
-          buffers = {
-            layout = { preset = "ivy", layout = { height = 0.3 } },
-          },
-          diagnostics = {
-            layout = { preset = "ivy", layout = { height = 0.3 } },
-          }
+          icons = { files = { enabled = true } },
         },
-        icons = { files = { enabled = false } }
-      }
-    },
-    keys = {
+      })
+
+      local map = vim.keymap.set
       -- Finding, Listing
-      { "<leader>f",         function() Snacks.picker.files({ cmd = "fd" }) end,                                                    desc = "Find Files" },
-      { "<leader>um",        function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,                                desc = "Find Config File" },
-      { "<leader>ud",        function() Snacks.picker.files({ cwd = "~/.config" }) end,                                             desc = "Find Dotfiles" },
-      { "<leader>bl",        function() Snacks.picker.buffers() end,                                                                desc = "List Buffers" },
-      { "<leader>bt",        function() Snacks.picker.treesitter() end,                                                             desc = "Buffer Treesitter Symbols" },
-      { "<leader>dl",        function() Snacks.picker.diagnostics() end,                                                            desc = "List Diagnostics" },
-      { "<leader><leader>l", function() Snacks.picker.projects() end,                                                               desc = "List Projects" },
+      map("n", "<leader>f", function() Snacks.picker.files({ cmd = "fd", hidden = true, ignored = true }) end, { desc = "Find (Project) Files" })
+      map("n", "<C-p>", function() Snacks.picker.files({ cmd = "fd" }) end, { desc = "Find (Project) Files" })
+      map("n", "<leader>um", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, { desc = "Find Config File" })
+      map("n", "<leader>ud", function() Snacks.picker.files({ cwd = "~/.config" }) end, { desc = "Find Dotfiles" })
+      map("n", "<leader>bl", function() Snacks.picker.buffers() end, { desc = "List Buffers" })
+      map("n", "<leader>bt", function() Snacks.picker.treesitter() end, { desc = "Buffer Treesitter Symbols" })
+      map("n", "<leader>dl", function() Snacks.picker.diagnostics() end, { desc = "List Diagnostics" })
+      map("n", "<leader><leader>l", function() Snacks.picker.projects() end, { desc = "List Projects" })
       -- Grep
-      { "gl",                function() Snacks.picker.lines({ layout = { preset = "ivy", layout = { height = 0.3 } } }) end,        desc = "Goto line" },
-      { "gL",                function() Snacks.picker.grep_buffers({ layout = { preset = "ivy", layout = { height = 0.3 } } }) end, desc = "Goto line-multi" },
-      { "<leader>/",         function() Snacks.picker.grep_word({ search = vim.fn.input("Grep > ") }) end,                          desc = "Grep on user input" },
-      { "<leader><leader>/", function() Snacks.picker.grep() end,                                                                   desc = "Live Grep" },
-      {
-        "<leader>*",
-        function()
-          Snacks.picker.grep_word()
-        end,
-        mode = { "n", "x" },
-        desc = "Grep word under cursor/visual"
-      },
-      { "<leader><leader>*", function() Snacks.picker.grep_word({ search = vim.fn.expand("<cWORD>") }) end, desc = "Grep WORD under cursor" },
+      map("n", "gl", function() Snacks.picker.lines() end, { desc = "Goto line" })
+      map("n", "gL", function() Snacks.picker.grep_buffers() end, { desc = "Goto line-multi" })
+      map("n", "<leader>/", function() Snacks.picker.grep_word({ search = vim.fn.input("Grep > ") }) end, { desc = "Grep on user input" })
+      map("n", "<leader><leader>/", function() Snacks.picker.grep() end, { desc = "Live Grep" })
+      map({ "n", "x" }, "<leader>*", function() Snacks.picker.grep_word() end, { desc = "Grep word under cursor/visual" })
+      map("n", "<leader><leader>*", function() Snacks.picker.grep_word({ search = vim.fn.expand("<cWORD>") }) end, { desc = "Grep WORD under cursor" })
       -- Git
-      { "<leader><leader>f", function() Snacks.picker.git_files() end,                                      desc = "Find Git Files" },
-      { "<leader>gc",        function() Snacks.picker.git_log() end,                                        desc = "Git Commits(Log)" },
-      { "<leader>gb",        function() Snacks.picker.git_branches() end,                                   desc = "Git Branches" },
-      { "<leader>gt",        function() Snacks.picker.git_status() end,                                     desc = "Git Status" },
-      { "<leader>gh",        function() Snacks.picker.git_stash() end,                                      desc = "Git Stash" },
+      map("n", "<leader><leader>f", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
+      map("n", "<leader>gc", function() Snacks.picker.git_log() end, { desc = "Git Commits(Log)" })
+      map("n", "<leader>gb", function() Snacks.picker.git_branches() end, { desc = "Git Branches" })
+      map("n", "<leader>gt", function() Snacks.picker.git_status() end, { desc = "Git Status" })
+      map("n", "<leader>gh", function() Snacks.picker.git_stash() end, { desc = "Git Stash" })
       -- File Tree
-      { "<leader>ut",        function() Snacks.picker.explorer() end,                                       desc = "File Tree Explorer" },
+      map("n", "<leader>ut", function() Snacks.picker.explorer() end, { desc = "File Tree Explorer" })
       -- Help
-      { "<leader>?t",        function() Snacks.picker.help() end,                                           desc = "Show Help Tags" },
-      { "<leader>?k",        function() Snacks.picker.keymaps() end,                                        desc = "Show Keymaps" },
-      { "<leader>?c",        function() Snacks.picker.commands() end,                                       desc = "Show Commands" },
-      { "<leader>?h",        function() Snacks.picker.command_history() end,                                desc = "Show Command History" },
-      { "<leader>?m",        function() Snacks.picker.man() end,                                            desc = "Show Man Pages" },
-      { "<leader>?j",        function() Snacks.picker.jumps() end,                                          desc = "Show Jump Lists" },
-      { "<leader>?l",        function() Snacks.picker.loclist() end,                                        desc = "Show Location List" },
-      { "<leader>?s",        function() Snacks.picker.search_history() end,                                 desc = "Show Search History" },
-      { "<leader>?r",        function() Snacks.picker.registers() end,                                      desc = "Show Registers" },
-      { "<leader>?M",        function() Snacks.picker.marks() end,                                          desc = "Show Marks" },
-    },
+      map("n", "<C-h>", function() Snacks.picker.help() end, { desc = "Show Help Tags" })
+      map("n", "<leader>?k", function() Snacks.picker.keymaps() end, { desc = "Show Keymaps" })
+      map("n", "<leader>?c", function() Snacks.picker.commands() end, { desc = "Show Commands" })
+      map("n", "<leader>?h", function() Snacks.picker.command_history() end, { desc = "Show Command History" })
+      map("n", "<leader>?m", function() Snacks.picker.man() end, { desc = "Show Man Pages" })
+      map("n", "<leader>?j", function() Snacks.picker.jumps() end, { desc = "Show Jump Lists" })
+      map("n", "<leader>?l", function() Snacks.picker.loclist() end, { desc = "Show Location List" })
+      map("n", "<leader>?s", function() Snacks.picker.search_history() end, { desc = "Show Search History" })
+      map("n", "<leader>?r", function() Snacks.picker.registers() end, { desc = "Show Registers" })
+      map("n", "<leader>?M", function() Snacks.picker.marks() end, { desc = "Show Marks" })
+    end,
   }
 }
