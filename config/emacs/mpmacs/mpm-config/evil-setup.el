0c9b93c5cd5ec2a8375578932b15d4c8ba66cebf
marinov98 <wbbmarin5@gmail.com>
2025-10-05 23:23:45 -0400

    feat(vscode, emacs): roughly same mc behavior + prev match

--- config/emacs/mpmacs/mpm-config/evil-setup.el
+++ config/emacs/mpmacs/mpm-config/evil-setup.el
@@ -132,7 +132,7 @@
         ("r" evil-mc-resume-cursors "resume cursors" :color blue))
       )
     )
-    (evil-global-set-key 'visual (kbd "C-n") 'hydra-mc/body)
+    (evil-global-set-key 'visual (kbd "C-n") 'hydra-mc/evil-mc-make-and-goto-next-match)
 )
 
 
