25a4878ff6f4850bea5eb0eb419aa1117bf3390b
Marin Marinov <mmarinov7@gatech.edu>
2025-10-07 21:20:10 -0400

    feat(nvim): room to customize diagnostic signs

--- config/nvim/lua/marinvim/plugins/lsp-setup.lua
+++ config/nvim/lua/marinvim/plugins/lsp-setup.lua
@@ -42,6 +42,14 @@ return {
 
 
       vim.diagnostic.config({
+        -- signs = {
+        --   text = {
+        --     [vim.diagnostic.severity.ERROR] = "",
+        --     [vim.diagnostic.severity.WARN]  = "",
+        --     [vim.diagnostic.severity.INFO]  = "",
+        --     [vim.diagnostic.severity.HINT]  = "",
+        --   },
+        -- },
         virtual_text = true,
         float = {
           focusable = false,
