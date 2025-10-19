b577d6acb5f8c5d8c04f0cbd05326f98f1277d20
Marin Marinov <mmarinov7@gatech.edu>
2025-10-19 19:36:34 -0400

    refactor(nvim): cm obsolete, quickfix bindings

--- config/nvim/lua/marinvim/preferences/keymaps.lua
+++ config/nvim/lua/marinvim/preferences/keymaps.lua
@@ -9,13 +9,15 @@ vim.keymap.set('n', '<leader>x', ":tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set('n', '<leader>q', ":q<CR>", { desc = "Quit" })
 vim.keymap.set('n', '<leader>Q', ":q!<CR>", { desc = "Force Quit" })
 
+-- QuickFix
+vim.keymap.set("n", "<C-;>o", "<cmd>copen<CR>", { desc = "QuickFix Open" })
+vim.keymap.set("n", "<C-;>x", "<cmd>cclose<CR>", { desc = "QuickFix Close" })
+vim.keymap.set("n", "<C-;>d", ":cdo ", { desc = "execute cdo" })
+vim.keymap.set("n", "<C-;>s", "<cmd>cdo :w<CR>", { desc = "execute cdo :w" })
+
 -- Coding utility
 vim.keymap.set('n', '<leader>cs', ":terminal<CR>", { desc = "Open terminal" })
 vim.keymap.set('n', '<leader>cn', ":noh<CR>", { desc = "Remove highlight" })
-vim.keymap.set("n", "<leader>cm", ":%s//gc<Left><Left><Left>",
-  { desc = "Search/replace in current file with confirmation" })     -- attempt at native multiple cursors
-vim.keymap.set("v", "<leader>cm", ":s//gc<Left><Left><Left>",
-  { desc = "Search/replace in visual highlight with confirmation" }) -- attempt at native multiple cursors visual mode
 vim.keymap.set("n", "<leader>ci", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = "Case insensitive search/replace under cursor for current file" })
 
