f182ad0e0291ce27aaae9067aed1a1d9c9f0ae41
Marin Marinov <mmarinov7@gatech.edu>
2025-10-24 10:13:12 -0400

    refactor(nvim): linting formatting together with lsp

--- config/nvim/lua/marinvim/plugins/lsp-setup.lua
+++ config/nvim/lua/marinvim/plugins/lsp-setup.lua
@@ -100,5 +100,57 @@ return {
         end,
       })
     end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    keys = '<leader>cl',
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        python = { "ruff" },
+      }
+      vim.keymap.set("n", "<leader>cl", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end
+  },
+  {
+    "stevearc/conform.nvim",
+    keys = { { '<leader>lf', mode = { 'n', 'v' } } },
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          python = { "ruff", "black" },
+          lua = { "stylua" },
+          svelte = { "prettier", "prettierd" },
+          javascript = { "prettier", "prettierd" },
+          typescript = { "prettier", "prettierd" },
+          javascriptreact = { "prettier", "prettierd" },
+          typescriptreact = { "prettier", "prettierd" },
+          json = { "prettier", "prettierd", lsp_format = "fallback" },
+          graphql = { "prettier", "prettierd" },
+          java = { "google-java-format" },
+          markdown = { "prettier", "prettierd" },
+          html = { "prettier", "prettierd" },
+          css = { "prettier", "prettierd" },
+          scss = { "prettier", "prettierd" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+        },
+      })
+      vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+        conform.format({
+          lsp_fallback = true,
+          async = true,
+          timeout_ms = 800,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
   }
 }
