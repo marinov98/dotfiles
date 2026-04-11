7be008af1f6e84a983c92d04cbff1b4c882c3639
Marin Marinov <mmarinov7@gatech.edu>
2026-04-11 22:07:27 +0300

    feat: nvim 0.12 new treesitter setup

--- config/nvim/lua/marinvim/plugins/nvim-treesitter.lua
+++ config/nvim/lua/marinvim/plugins/nvim-treesitter.lua
@@ -1,44 +1,56 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {
-          -- mandatory (keep these as recommended by documentation)
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          -- preference (personal choice starts here)
-          "python",
-          "cpp",
-          "java",
-          "elixir",
-          "go",
-          "sql",
-          "rust",
-          -- config
-          "kdl", -- for zellij
-          "xml",
-          "cmake",
-          "yaml",
-          "toml",
-          -- Web Dev
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "html",
-          "css",
-          "scss"
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
+    init = function()
+      -- Install missing parsers without reinstalling already-installed ones
+      local ensureInstalled = {
+        -- mandatory
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        -- preference
+        "python",
+        "cpp",
+        "java",
+        "elixir",
+        "go",
+        "sql",
+        "rust",
+        -- config
+        "kdl",
+        "xml",
+        "cmake",
+        "yaml",
+        "toml",
+        -- Web Dev
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "scss",
+      }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require("nvim-treesitter").install(parsersToInstall)
+
+      -- Enable highlighting and indentation via FileType autocmd
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
-    end
+    end,
   }
 }
