0803d001e7ad4daa6cdd87d05c4aefa04cc8b8e2
Marin Marinov <mmarinov7@gatech.edu>
2026-05-05 21:57:59 -0400

    docs: keep ripgrep option ready

--- config/shell/common.sh
+++ config/shell/common.sh
@@ -32,9 +32,15 @@ alias path='echo -e ${PATH//:/\\n}'
 
 CUSTOM_PROJECTS_DIR_PATH="$HOME/projects/"
 
+# Ripgrep
+# export FZF_DEFAULT_COMMAND='rg --files --hidden -g "!{.git}"'
+# alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(find . -type d -print | fzf)' 
+
+
 # Fd
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(fd . -t d | fzf)'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 alias vz='v $(fzf)'
+
