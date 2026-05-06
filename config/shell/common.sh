650b36da7a6ac8875f7ae4d647cfd61280116a47
Marin Marinov <mmarinov7@gatech.edu>
2026-05-05 20:11:48 -0400

    refactor(shell): extract common config and add shell detection
    - Add config/shell/ with common.sh (shared aliases, FZF, EDITOR)
    - Add config/shell/zshrc and config/shell/bashrc sourcing common.sh
    - Add config/shell/bash_profile for bash login shells
    - Update omni_configurator.sh to detect /bin/zsh and move configs to home
    - Remove duplicate shell/bash/ and shell/zsh/ directories

---
+++ config/shell/common.sh
@@ -0,0 +1,36 @@
+###################################
+####### EDITOR
+###################################
+
+export EDITOR=nvim
+export VISUAL=nvim
+
+###################################
+####### Aliases
+###################################
+
+alias ls='ls -GFh --color=auto'
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias mkdir='mkdir -pv'
+alias ll='ls -l'
+alias less='less -FSRXc'
+
+alias v='nvim'
+alias vc='v --clean'
+alias ec='emacsclient -n -c -a ""'
+alias zj='zellij'
+alias path='echo -e ${PATH//:/\\n}'
+
+###################################
+####### FZF
+###################################
+
+CUSTOM_PROJECTS_DIR_PATH="$HOME/projects/"
+
+# Fd
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(fd . -t d | fzf)'
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+alias vz='v $(fzf)'
--- scripts/omni_configurator.sh
+++ scripts/omni_configurator.sh
@@ -3,17 +3,31 @@ copy_dotfiles_to_home() {
   echo "Certain shell configurations will be copied based on the system"
   if [[ $(uname -s) == Linux ]] || [[ $(uname -s) == Darwin ]]
   then
-      echo "machine found to be linux or Mac"
-      echo "copying shell .."
-      \cp shell/zsh/.zshrc ~/
-      echo "shell done"
-      echo "copying config..."
-      \cp -r config/* ~/.config/
-      echo "creating '.ignore' from fzyIgnore..."
-      mv ~/.config/fzyIgnore ~/.ignore
-      echo "Finished setting up config"
+    echo "machine found to be linux or Mac"
+    echo "copying config..."
+    cp -r config/* ~/.config/
+    echo "creating '.ignore' from fzyIgnore..."
+    mv ~/.config/fzyIgnore ~/.ignore
+    echo "Finished setting up config"
+
+    echo "detecting shell and copying shell config..."
+    case "$SHELL" in
+      */zsh)
+        mv ~/.config/shell/zshrc ~/.zshrc
+        echo "zsh config copied"
+        ;;
+      */bash)
+        mv ~/.config/shell/bashrc ~/.bashrc
+        mv ~/.config/shell/bash_profile ~/.bash_profile
+        echo "bash config copied"
+        ;;
+      *)
+        echo "Unsupported shell: $SHELL"
+        ;;
+    esac
+    echo "shell done"
   else
-      echo "Machine found to not be linux or Mac will not copy config!"
+    echo "Machine found to not be linux or Mac will not copy config!"
   fi
 
   echo "copying git configs..."
@@ -34,4 +48,3 @@ setup_zsh_git_completion() {
   curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
   curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 }
-
