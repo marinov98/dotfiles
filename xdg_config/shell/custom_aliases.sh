a41e199a0abe6f15d3cae372846b0f1ea4f350ee
Marin Marinov <mmarinov7@gatech.edu>
2026-05-06 22:21:53 -0400

    refactor(shell): max depth 2

--- xdg_config/shell/custom_aliases.sh
+++ xdg_config/shell/custom_aliases.sh
@@ -26,12 +26,12 @@ CUSTOM_PROJECTS_DIR_PATH="$HOME/projects/"
 
 # Ripgrep
 # export FZF_DEFAULT_COMMAND='rg --files --hidden -g "!{.git}"'
-# alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(find . -type d -print | fzf)' 
+# alias zfd='cd "$CUSTOM_PROJECTS_DIR_PATH" && cd "$(find . -maxdepth 2 -type d | fzf)"'
 
 
 # Fd
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
-alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(fd . -t d | fzf)'
+alias zfd='cd $CUSTOM_PROJECTS_DIR_PATH && cd $(fd . -d 2 -t d | fzf)'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 alias vz='v $(fzf)'
