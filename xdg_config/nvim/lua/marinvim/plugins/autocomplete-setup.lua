3e2cef16e6958796e9aece0b35a0c96b2644032c
Marin Marinov <mmarinov7@gatech.edu>
2026-05-11 21:52:59 -0400

    feat(nvim): nvim lua autocomplete

--- xdg_config/nvim/lua/marinvim/plugins/autocomplete-setup.lua
+++ xdg_config/nvim/lua/marinvim/plugins/autocomplete-setup.lua
@@ -4,6 +4,7 @@ return {
     version = "v1.*",
     dependencies = {
       "rafamadriz/friendly-snippets", -- useful snippets
+      { "folke/lazydev.nvim", ft = "lua", opts = {} },
     },
     opts = {
       signature = { enabled = true, window = { border = "rounded" } },
@@ -17,7 +18,18 @@ return {
         },
       },
       sources = {
-        min_keyword_length = 2
+        min_keyword_length = 2,
+        per_filetype = {
+          lua = { inherit_defaults = true, 'lazydev' }
+        },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
     }
   }
