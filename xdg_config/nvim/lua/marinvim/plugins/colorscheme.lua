31c31f71ef9f09e9f1dbf7d0d4f6965f8d91f590
Marin Marinov <mmarinov7@gatech.edu>
2026-06-07 10:39:15 -0400

    refactor(nvim): is this the colorscheme config I've been looking for?

--- xdg_config/nvim/lua/marinvim/plugins/colorscheme.lua
+++ xdg_config/nvim/lua/marinvim/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
+local theme = "cyberdream"
 return {
   {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
     },
+    enabled = theme == "tokyonight",
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require(theme).setup(opts)
+      vim.cmd.colorscheme(theme)
+    end
   },
   {
     "catppuccin/nvim",
@@ -11,13 +19,41 @@ return {
     opts = {
       flavour = "mocha",
     },
+    enabled = theme == "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require(theme).setup(opts)
+      vim.cmd.colorscheme(theme)
+    end
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    opts = {},
+    enabled = theme == "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require(theme).setup(opts)
+      vim.cmd.colorscheme(theme)
+    end
   },
   {
     "EdenEast/nightfox.nvim",
+    opts = {},
+    enabled = theme == "carbonfox" or
+        theme == "duskfox" or
+        theme == "nightfox" or
+        theme == "terafox" or
+        theme == "dayfox" or
+        theme == "nordfox",
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require("nightfox").setup(opts)
+      vim.cmd.colorscheme(theme)
+    end
   },
   {
     "scottmckendry/cyberdream.nvim",
@@ -25,11 +61,12 @@ return {
       variant = "dark",
       cache = true
     },
+    enabled = theme == "cyberdream",
     lazy = false,
     priority = 1000,
     config = function(_, opts)
-      require("cyberdream").setup(opts)
-      vim.cmd.colorscheme("cyberdream")
+      require(theme).setup(opts)
+      vim.cmd.colorscheme(theme)
     end
   }
 }
