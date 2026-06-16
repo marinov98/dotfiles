59ff26e798620617f661929ca3761d4e20377a69
Marin Marinov <wbbmarin5@gmail.com>
2026-06-15 22:11:35 -0400

    fix(brew): update Homebrew installer to use bash
    Replace deprecated Ruby-based installation with current bash-based command
    from brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,7 +2,7 @@
 
 brew_install_mini() {
   xcode-select --install
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install curl ripgrep fzf
   brew install font-fira-code font-jetbrains-mono
 }
